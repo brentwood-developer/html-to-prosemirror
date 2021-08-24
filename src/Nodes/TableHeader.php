2a3ffc2a329a276ceaeb121e54d3341c7ac798c4
brentwood-developer <84422067+brentwood-developer@users.noreply.github.com>
2021-08-24 12:51:10 -0700

    Update TableHeader.php

--- src/Nodes/TableHeader.php
+++ src/Nodes/TableHeader.php
@@ -5,5 +5,5 @@ namespace HtmlToProseMirror\Nodes;
 class TableHeader extends TableCell
 {
     protected $tagName = 'th';
-    protected $nodeType = 'table_header';
+    protected $nodeType = 'tableHeader';
 }
